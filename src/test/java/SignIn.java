97d2bb2268b823ff89891f03dedd9851f19da320
CzarekCzapczynski <czarek.czapczynski@gmail.com>
2018-04-24 13:23:34 +0200

    add method login

--- src/test/java/SignIn.java
+++ src/test/java/SignIn.java
@@ -1,2 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class SignIn {
+    WebDriver driver = new ChromeDriver();
+
+    public void login (String userName, String password) {
+        driver.findElement(By.cssSelector("[name='username']")).sendKeys(userName);
+        driver.findElement(By.cssSelector("[name='password']")).sendKeys(password);
+        driver.findElement(By.cssSelector("div .login .button")).click();
+    }
+
+
 }
