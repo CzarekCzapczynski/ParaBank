2f0a8445fc5321a656660aba13edabc3e33175fe
CzarekCzapczynski <czarek.czapczynski@gmail.com>
2018-04-24 13:45:25 +0200

    add method waitForJStoLoad

--- src/test/java/SignIn.java
+++ src/test/java/SignIn.java
@@ -1,13 +1,42 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SignIn {
     WebDriver driver = new ChromeDriver();
+
+    public boolean waitForJStoLoad() {
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+
+        try {
+            Thread.sleep(100);  //sleep 100ms to be sure there is no delay in browser befor kicking of jquery or js
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // wait for jQuery to load
+        ExpectedCondition<Boolean> jQueryLoad = new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                try {
+                    return ((Long) ((JavascriptExecutor) driver).executeScript("return jQuery.active") == 0);
+                } catch (Exception e) {
+                    return true;
+                }
+            }
+        };
+
+        // wait for Javascript to load
+        ExpectedCondition<Boolean> jsLoad = new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                return ((JavascriptExecutor) driver).executeScript("return document.readyState")
+                        .toString().equals("complete");
+            }
+        };
+        return wait.until(jQueryLoad) && wait.until(jsLoad);
+    }
 
     public void login (String userName, String password) {
         driver.findElement(By.cssSelector("[name='username']")).sendKeys(userName);
@@ -18,6 +47,7 @@ public class SignIn {
     @Test(priority = 1)
     public void shouldLogin() {
         driver.get("http://parabank.parasoft.com");
+        waitForJStoLoad();
         login("test", "test");
     }
 
