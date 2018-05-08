32310a2aef28b287c90c9aa43123c0cfa4f12b66
CzarekCzapczynski <czarek.czapczynski@gmail.com>
2018-05-08 13:55:52 +0200

    LookupPage ForgotPasswordTest

--- src/main/java/pages/LookupPage.java
+++ src/main/java/pages/LookupPage.java
@@ -3,11 +3,59 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class LookupPage extends RegisterPage {
+public class LookupPage extends MainPage{
     public LookupPage(WebDriver driver) {
         super(driver);
     }
+
+    public void openLookupPage() {
+        driver.get("http://parabank.parasoft.com/parabank/lookup.htm");
+    }
+
+    public void setFirstName(String firstName){
+        driver.findElement(By.cssSelector("#firstName")).sendKeys(firstName);
+    }
+
+    public void setLastName(String lastName) {
+        driver.findElement(By.cssSelector("#lastName")).sendKeys(lastName);
+    }
+
+    public void setStreet(String street) {
+        driver.findElement(By.cssSelector("[id$='street']")).sendKeys(street);
+    }
+
+    public void setCity(String city) {
+        driver.findElement(By.cssSelector("[id$='city']")).sendKeys(city);
+    }
+
+    public void setState(String state) {
+        driver.findElement(By.cssSelector("[id$='state']")).sendKeys(state);
+    }
+
+    public void setZipCode(String zipCode) {
+        driver.findElement(By.cssSelector("[id$='zipCode']")).sendKeys(zipCode);
+    }
+
+    public void setSsn(String ssn) {
+        driver.findElement(By.cssSelector("#ssn")).sendKeys(ssn);
+    }
+
     public void clickFindMyLoginInfoButton() {
         driver.findElement(By.cssSelector("[value^='Find']")).click();
+    }
+
+    public void fillLookupForm(String firstName, String lastName,
+                                 String street, String city, String state, String zipCode, String ssn) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setStreet(street);
+        setCity(city);
+        setState(state);
+        setZipCode(zipCode);
+        setSsn(ssn);
+    }
+
+    public boolean isExecutionPassed(By by) {
+        return driver.findElement(by).isDisplayed();
     }
 }
--- src/test/java/ForgotPasswordTest.java
+++ src/test/java/ForgotPasswordTest.java
@@ -1,11 +1,32 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import pages.AccountPage;
 import pages.LookupPage;
 
 public class ForgotPasswordTest {
-    public class LoginTest {
-        WebDriver driver;
-        LookupPage lookupPage;
-        AccountPage accountPage;
+    WebDriver driver;
+    LookupPage lookupPage;
+    AccountPage accountPage;
+
+    @BeforeMethod
+    public void before(){
+        driver = new ChromeDriver();
+        lookupPage = new LookupPage(driver);
+        accountPage = new AccountPage(driver);
+    }
+
+    @Test
+    public void shouldFindCustomer() {
+        lookupPage.openLookupPage();
+        lookupPage.waitForJStoLoad();
+        lookupPage.fillLookupForm("Ogórek", "Szklarniowy",
+                "Ogórkowa", "Grządki", "Pole", "0000", "11");
+        lookupPage.clickFindMyLoginInfoButton();
+        lookupPage.waitForJStoLoad();
+        Assert.assertTrue(lookupPage.isExecutionPassed(By.cssSelector("a[href$='logout.htm']")));
     }
 }
